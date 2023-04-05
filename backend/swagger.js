7edc5b50f5f78359705dade333bc3a58c17d4619
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-04-06 03:03:31 +0530

    backend completed

---
+++ backend/swagger.js
@@ -0,0 +1,26 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Booking API',
+      version: '1.0.0',
+      description: 'API for managing bookings',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Local server',
+      },
+    ],
+  },
+  apis: ['  /booking.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = (app) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
