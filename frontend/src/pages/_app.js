a3e7eec5b7fc33ced9b5a49e4ec81d7504f108c2
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-04-05 19:00:37 +0530

    animate.css added as a global css dependencies

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,13 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import 'animate.css';
+import '@/styles/index.css'
+import Layout from './components/Layout.js';
+import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+  <>
     <ChakraProvider>
     <Layout>
     <Component {...pageProps} />
     </Layout>
     </ChakraProvider> 
-    </>
+  </>
   )
 }
