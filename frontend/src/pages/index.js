1e1dbc7147eb7a234aca85ac6ce5fe1e21f565a1
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-04-06 09:21:41 +0530

    quotes removed from poages/index

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
     <>
   <div id='homepage' style={{textAlign:"center"}}>
   <h1>Welcome To Travelopia</h1>
-  <p>Click on <Link href={"travel"}><b>"Travel"</b></Link>  to book your travel destination</p>
-  <p>Click on <Link href={"booking"}><b>"Active Booking"</b></Link>  to see your Active Booking</p>
+  <p>Click on <Link href={"travel"}><b>Travel</b></Link>  to book your travel destination</p>
+  <p>Click on <Link href={"booking"}><b>Active Booking</b></Link>  to see your Active Booking</p>
   </div>  
   </>
   )
